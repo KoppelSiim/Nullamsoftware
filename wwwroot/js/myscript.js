0e42ebb5a29e9736cf008666aaf5c893414d0fbe
Siim Koppel <siimk999@gmail.com>
2022-12-17 17:44:46 +0200

    Add project files.

---
+++ wwwroot/js/myscript.js
@@ -0,0 +1,10 @@
+﻿function hover(color,color2) {
+    document.getElementById('testing').style.backgroundColor = color;
+    document.getElementById('testing2').style.color = color2;
+ 
+}   
+function hover2(color, color2) {
+    document.getElementById('navli').style.backgroundColor = color;
+    document.getElementById('nava').style.color = color2;
+
+}   
